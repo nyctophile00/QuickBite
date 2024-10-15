b2402829cf52fef1093e76ae10832b5ecac6e8ea
Nazmul Islam Chowdhury <nazmul2004@protonmail.com>
2024-10-15 09:16:54 +0600

    home

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Restaurants from './pages/Restaurants.jsx';
+import Header from './components/Header.jsx';
 
 
 createRoot(document.getElementById('root')).render(
---
+++ src/pages/Restaurants.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function Restaurants() {
+  return (
+    <div>Restaurants</div>
+  )
+}
